e7849c1cc611a8085869ca9c9c313c7891d4e70c
Krishna Gunjal <krishna.gunjal@nitorinfotech.com>
2023-03-08 12:49:29 +0530

    Added text-to-speech functions.

--- Text-to-Speech/Text-to-Speech/ViewController.swift
+++ Text-to-Speech/Text-to-Speech/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
+    @IBOutlet weak var speakLabel: UILabel!
+    @IBOutlet weak var speakButton: UIButton!
+    
+    var speechText = ["one two, three four!  five six.  $seven?"]
+    var count = 0
+    var speechSynthesizer = AVSpeechSynthesizer()
+    
+    func pauseTheSpeech() {
+        speechSynthesizer.stopSpeaking(at: .immediate)
+    }
+    
+    func startThespeech() {
+        if speechSynthesizer.isSpeaking {
+            speechSynthesizer.stopSpeaking(at: .immediate)
+        }else {
+            let speechUterence = AVSpeechUtterance(string: speechText[count])
+            DispatchQueue.main.async {
+                self.speechSynthesizer.speak(speechUterence)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        speechSynthesizer.delegate = self
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        speakLabel.text = utterance.speechString
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        speechSynthesizer.stopSpeaking(at: .word)
+        
+        count += 1
+        if (count < speechText.count) {
+            _ = AVSpeechUtterance(string: (speechText[count]))
+        }else {
+            count = 0
+            speakButton.isSelected = false
+        }
     }
 
-
+    @IBAction func speak(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected {
+            startThespeech()
+        } else{
+            pauseTheSpeech()
+        }
+    }
+    
 }
 
